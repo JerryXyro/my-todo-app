e220e777f6512e72ed2ec8eba8c3827c57d86c88
JerryXyro <jerryprince981@gmail.com>
2020-12-12 18:41:52 +0530

    chnaged bg color, added button colors for visual interactivity

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,15 @@ import React, { useState } from "react";
 const Form = (props) => {
   const [list_name, setName] = useState("");
 
+  function changeColorEnter() {
+    let btn = document.getElementById("button_color_hover");
+    btn.style.backgroundColor = list_name.length > 0 ? "green" : "red";
+  }
+
+  function changeColorLeave() {
+    let btn = document.getElementById("button_color_hover");
+    btn.style.backgroundColor = "#ffffff";
+  }
   function handleChange(e) {
     setName(e.target.value);
     console.log(list_name);
@@ -21,7 +30,14 @@ const Form = (props) => {
         onChange={handleChange}
         autoComplete="off"
       />
-      <button type="submit">Add</button>
+      <button
+        type="submit"
+        id="button_color_hover"
+        onMouseEnter={changeColorEnter}
+        onMouseLeave={changeColorLeave}
+      >
+        Add
+      </button>
     </form>
   );
 };
